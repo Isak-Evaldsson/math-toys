708df2a43dfc2c2d59ad816a4cd381fbdad67032
Isak-Evaldsson <isak.evaldsson@outlook.com>
2020-07-02 12:57:35 +0200

    Can parse addition.

--- src/parser.js
+++ src/parser.js
@@ -1,3 +1,16 @@
-export function parse(string) {
-    return string;
+/*
+    TODO:
+    * Regex input filtering
+    * Error detection
+*/
+
+export function parse(inputString) {
+    const cleanInput = inputString.replaceAll(' ', '')
+    return parseAdditon(cleanInput)
+}
+
+function parseAdditon(string) {
+    const numbers = string.split('+').map(x => parseInt(x))
+
+    return numbers.reduce((a,b) => a + b, 0)
 }
