7517439ceb68295f3b3b07e00d0599ed8d84daa8
Isak-Evaldsson <isak.evaldsson@outlook.com>
2020-07-05 15:23:47 +0200

    Parser handles basic arithmetic and parens.

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,9 @@
 /*
-    TODO:
+    Improvements:
     * Regex input filtering
     * Error detection
+    * Proper abstract syntax tree
+    * Lexer
 */
 
 export function parse(inputString) {
@@ -10,7 +12,55 @@ export function parse(inputString) {
 }
 
 function parseAdditon(string) {
-    const numbers = string.split('+').map(x => parseInt(x))
+    const terms = split(string, '+').map(expr => parseSubtraction(expr))
 
-    return numbers.reduce((a,b) => a + b, 0)
+    return terms.reduce((a,b) => a + b, 0)
+}
+
+function parseSubtraction(string) {
+    const terms = split(string, '-').map(expr => parseMultiplication(expr))
+
+    return terms.slice(1).reduce((a,b) => a - b, terms[0])
+}
+
+function parseMultiplication(string) {
+    const factors = split(string, '*').map(x => parseDivision(x))
+
+    return factors.reduce((a,b) => a * b, 1) 
+}
+
+function parseDivision(string) {
+    const factors = split(string, '/').map(x => {
+        if (x[0] === '(') {
+            return parseAdditon(x.slice(1,x.length - 1))
+        }
+        return parseInt(x)
+    })
+
+    return factors.slice(1).reduce((a,b) => a / b, factors[0])
+}
+
+function split(expr, operator) {
+    const result = []
+    let parenthisisLevel = 0
+    let currentChunck = ""
+
+    for (let i = 0; i < expr.length; i++) {
+        const currentChar = expr[i];
+        if(currentChar === '(') {
+            parenthisisLevel++
+        } else if(currentChar === ')'){
+            parenthisisLevel--
+        }
+
+        if(parenthisisLevel === 0 && currentChar === operator) {
+            result.push(currentChunck)
+            currentChunck = ""
+        } else currentChunck += currentChar
+    }
+    if(currentChunck !== "") {
+        result.push(currentChunck)
+    }
+
+    return result
 }
