348dd4aa215e1285899feb392bd4c4c2206afd82
Isak-Evaldsson <isak.evaldsson@outlook.com>
2020-07-06 11:29:13 +0200

    Added some css styling.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {parse} from './parser.js'
+import './index.css'
 
 class Calulcator extends React.Component {
     constructor(props){
@@ -10,9 +11,9 @@ class Calulcator extends React.Component {
         }
     }
 
-    createCalcButton(operator) {
+    createOperatorButton(operator, type = 'numBtn') {
         return (
-        <button onClick={() => {
+        <button class={type} onClick={() => {
             const newExpr = this.state.expr + operator
             this.setState({expr: newExpr})
         }}>
@@ -20,34 +21,53 @@ class Calulcator extends React.Component {
         </button>
         )
     }
+
+    createFunctionButton(lambda, sign, type='opBtn') {
+        return (
+            <button class={type} onClick={lambda}>
+                {sign}
+            </button>
+        )
+    }
    
     render() {
+        const caluclate = () => this.setState({expr: parse(this.state.expr).toString(10)})
+        const clear = () => this.setState({expr: this.state.expr.substring(0, this.state.expr.length - 1)})
+
         return (
             <div>
+                
+                <div id="labelDiv">
                 <label>{this.state.expr}</label>
-                <div>
-                    {this.createCalcButton('7')}
-                    {this.createCalcButton('8')}
-                    {this.createCalcButton('9')}
-                    {this.createCalcButton('*')}
                 </div>
                 <div>
-                    {this.createCalcButton('4')}
-                    {this.createCalcButton('5')}
-                    {this.createCalcButton('6')}
-                    {this.createCalcButton('/')}
+                    {this.createOperatorButton('(', 'opBtn')}
+                    {this.createOperatorButton(')', 'opBtn')}
+                    {this.createFunctionButton(clear, 'CE')}
                 </div>
                 <div>
-                    {this.createCalcButton('1')}
-                    {this.createCalcButton('2')}
-                    {this.createCalcButton('3')}
-                    {this.createCalcButton('-')}
+                    {this.createOperatorButton('7')}
+                    {this.createOperatorButton('8')}
+                    {this.createOperatorButton('9')}
+                    {this.createOperatorButton('*', 'opBtn')}
                 </div>
                 <div>
-                    {this.createCalcButton('0')}
-                    {this.createCalcButton('.')}
-                    <button onClick = {() => this.setState({expr: parse(this.state.expr)})}>=</button>
-                    {this.createCalcButton('+')}
+                    {this.createOperatorButton('4')}
+                    {this.createOperatorButton('5')}
+                    {this.createOperatorButton('6')}
+                    {this.createOperatorButton('/', 'opBtn')}
+                </div>
+                <div>
+                    {this.createOperatorButton('1')}
+                    {this.createOperatorButton('2')}
+                    {this.createOperatorButton('3')}
+                    {this.createOperatorButton('-', 'opBtn')}
+                </div>
+                <div>
+                    {this.createOperatorButton('0')}
+                    {this.createOperatorButton('.')}
+                    {this.createFunctionButton(caluclate, '=', 'eqBtn')}
+                    {this.createOperatorButton('+')}
                 </div>
             </div>
            
