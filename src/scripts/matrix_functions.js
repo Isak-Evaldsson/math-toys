0dda601fddb469998fad21f6a1f9ac575afe8d6b
Isak-Evaldsson <isak.evaldsson@outlook.com>
2020-08-03 14:04:45 +0200

    Added matric addition,

--- src/scripts/matrix_functions.js
+++ src/scripts/matrix_functions.js
@@ -8,4 +8,22 @@ function createMatrix(nbrOfRows, nbrOfCols, defaultValue = undefined) {
     return matrix
 }
 
-export {createMatrix}
+function addMatrices(a, b) {
+    if(a.length === 0 || b.length === 0) {
+        throw new Error("Can't add empty matrices")
+    } else if (a.length !== b.length || a[0].length !== b[0].length) {
+        throw new Error("Can't add matrices of different size.")        
+    }
+    
+    const res = createMatrix(a.length, a[0].length, 0)
+
+    for (let row = 0; row < res.length; row++) {
+        for (let col = 0; col < res[0].length; col++) {
+            res[row][col] = a[row][col] + b[row][col]
+        }
+    }
+
+    return res
+}
+
+export {createMatrix, addMatrices}
